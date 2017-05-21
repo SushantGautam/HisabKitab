43c35b2923858a2b326c596508ada2cc6d35f243
Sushant Gautam <susant.gautam@gmail.com>
2017-05-21 19:57:41 +0545

    git init

---
+++ app/src/main/java/madhushala/hisabkitab/HisabKitab.java
@@ -0,0 +1,13 @@
+package madhushala.hisabkitab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HisabKitab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hisab_kitab);
+    }
+}
