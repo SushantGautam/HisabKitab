fc4e6064fcec0dc4936a92091a70fac856cfa90d
Sushant Gautam <susant.gautam@gmail.com>
2017-05-22 21:28:08 +0545

    added a function to add a line to database

--- app/src/main/java/madhushala/hisabkitab/DBHelper.java
+++ app/src/main/java/madhushala/hisabkitab/DBHelper.java
@@ -2,6 +2,7 @@ package madhushala.hisabkitab;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -9,12 +10,10 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by eSushant on 5/22/2017.
  */
 
-public class DBHelper extends SQLiteOpenHelper
-{
+public class DBHelper extends SQLiteOpenHelper {
     public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
-
 
 
     @Override
@@ -23,8 +22,8 @@ public class DBHelper extends SQLiteOpenHelper
     // This is a raw sql command. We can call it using execSQL method
     public void onCreate(SQLiteDatabase db) {
         db.execSQL
-    (
-       "create table contacts " +"(id integer primary key, name text,amount text)" );
+                (
+                        "create table contacts " + "(id integer primary key, name text,amount text)");
     }
 
     @Override
@@ -33,7 +32,7 @@ public class DBHelper extends SQLiteOpenHelper
     }
 
     //This function will add a new row of data in the database
-    public boolean insertRow (String name, String amount) {
+    public boolean insertRow(String name, String amount) {
 
         SQLiteDatabase db = this.getWritableDatabase(); //Lets make a SQLiteDatabase object to use within this fuction
         ContentValues contentValues = new ContentValues(); //ContentValues class can store label-value pair
@@ -43,8 +42,18 @@ public class DBHelper extends SQLiteOpenHelper
         return true; //This is a boolean. will return 1 if success
     }
 
-
-
+    //Get all data from database using this function
+    // Cursor class  interface provides random read-write access to the result set returned by a database query.
+    //We can retrieve anything from database using an object of the Cursor class.
+    // We will call a method of this class called rawQuery and
+    // it will return a resultset with the cursor pointing to the table.
+    // We can move the cursor forward and retrieve the data.
+    public Cursor getData(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();  //getReadableDatabase is a method of SQLiteOpenHelper
+        // Thats why we used it to derive a class
+        Cursor data = db.rawQuery("select * from contacts where id=" + id + "", null); //The same rawquery as in oncreate method
+        return data; //finally it will return the data with given id as parameter
+    }
 
 
 
