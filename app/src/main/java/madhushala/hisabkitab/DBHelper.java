2607e9ef4539fb1f4a17d178c06afea83b17f39b
Sushant Gautam <susant.gautam@gmail.com>
2017-05-22 21:30:02 +0545

    added a function to add a line to database

--- app/src/main/java/madhushala/hisabkitab/DBHelper.java
+++ app/src/main/java/madhushala/hisabkitab/DBHelper.java
@@ -8,26 +8,20 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by eSushant on 5/22/2017.
  */
 
-public class DBHelper extends SQLiteOpenHelper
-{
+public class DBHelper extends SQLiteOpenHelper {
     public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
 
-
     @Override
-<<<<<<< Temporary merge branch 1
-    //We need to maken a database when this class is created to do so execute the following SQL command::
-=======
     //We need to make a database when this class is created to do so execute the following SQL command::
->>>>>>> Temporary merge branch 2
     // create table contacts( id integer primary key, name text,phone text,email text, street text,place text)
     // This is a raw sql command. We can call it using execSQL method
     public void onCreate(SQLiteDatabase db) {
         db.execSQL
-    (
-       "create table contacts " +"(id integer primary key, name text,phone text,email text, street text,place text)" );
+                (
+                        "create table contacts " + "(id integer primary key, name text,phone text,email text, street text,place text)");
     }
 
     @Override
